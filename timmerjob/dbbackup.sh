9b13390f05558aa3a1754653bb9ae8a6c955975e
chenlian <chenlian2025@gmail.com>
2015-04-29 11:50:09 +0800

    submit changes by admin

--- timmerjob/dbbackup.sh
+++ timmerjob/dbbackup.sh
@@ -1,5 +1,6 @@
-mysqldump -uroot -phello54987663 uh | gzip > /alidata/timmerjob/gagablogdatabase/$(date +%Y-%m-%d_%H%M%S).sql.gz
-cd  /alidata/timmerjob/gagablogdatabase/
+cd /alidata/www/default/timmerjob
+/alidata/server/mysql-5.5.37/bin/mysqldump -uroot -phello54987663 uh | /bin/gzip > /alidata/www/default/timmerjob/gagablogdatabase/$(date +%Y-%m-%d_%H%M%S).sql.gz
+cd  /alidata/www/default/timmerjob/gagablogdatabase/
 git add *
 git commit -m "back db gagablog" -a
 git push -u origin master
