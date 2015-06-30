ea337a2cb313fcd4eef966d2bc1ef384d78217c7
chenlian <chenlian2025@gmail.com>
2015-06-30 18:25:40 +0800

    submit changes by admin

---
+++ zenTao/tmp/log/php.20150630.log.php
@@ -0,0 +1,20 @@
+<?php
+ die();
+?>
+
+18:11:57 Undefined property: config::$debug in module/common/view/header.lite.html.php on line 20 when visiting /zenTao/www/install.php
+
+18:12:03 Undefined property: config::$debug in module/common/view/header.lite.html.php on line 20 when visiting /zenTao/www/install.php?m=install&f=license
+
+18:12:06 Undefined property: config::$debug in module/common/view/header.lite.html.php on line 20 when visiting /zenTao/www/install.php?m=install&f=step1
+
+18:12:11 Undefined property: config::$debug in module/common/view/header.lite.html.php on line 20 when visiting /zenTao/www/install.php?m=install&f=step2
+
+18:12:27 shell_exec(): Unable to execute 'which mysqldump' in module/install/model.php on line 487 when visiting /zenTao/www/install.php?m=install&f=step3
+
+18:12:27 Undefined property: config::$debug in module/common/view/header.lite.html.php on line 20 when visiting /zenTao/www/install.php?m=install&f=step3
+
+18:13:37 Undefined offset: 1 in module/user/control.php on line 686 when visiting /zenTao/www/index.php?m=user&f=login&referer=L3plblRhby93d3cvaW5kZXgucGhw
+
+18:24:57 ERROR: SQLSTATE[HY000] [2002] Connection refused in framework/router.class.php on line 1523, last called by framework/router.class.php on line 1485 through function connectByPDO.
+ in framework/router.class.php on line 1567 when visiting 
