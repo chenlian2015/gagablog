cb176840234ac91027526d58ac0d0d3adb1e67d6
chenlian <chenlian2025@gmail.com>
2015-07-02 00:01:14 +0800

    submit changes by admin

---
+++ zenTao/tmp/log/php.20150701.log.php
@@ -0,0 +1,45 @@
+<?php
+ die();
+?>
+
+10:44:01 Undefined property: stdClass::$password in module/user/model.php on line 369 when visiting /zenTao/www/index.php?m=user&f=edit&userID=2&from=company
+
+10:44:57 Undefined property: stdClass::$password in module/user/model.php on line 369 when visiting /zenTao/www/index.php?m=user&f=edit&userID=2&from=company
+
+10:53:26 Undefined property: router::$user in module/common/model.php on line 158 when visiting /zenTao/www/index.php?m=product&f=ajaxGetDropMenu&t=html&objectID=1&module=product&method=browse&extra=
+
+10:53:26 Trying to get property of non-object in module/common/model.php on line 158 when visiting /zenTao/www/index.php?m=product&f=ajaxGetDropMenu&t=html&objectID=1&module=product&method=browse&extra=
+
+11:00:42 Undefined property: stdClass::$password in module/user/model.php on line 369 when visiting /zenTao/www/index.php?m=user&f=edit&userID=5&from=company
+
+11:03:20 Undefined property: stdClass::$password in module/user/model.php on line 369 when visiting /zenTao/www/index.php?m=user&f=edit&userID=7&from=company
+
+12:15:11 Undefined index: root in module/product/view/view.html.php on line 74 when visiting /zenTao/www/index.php?m=product&f=view&productID=1
+
+12:15:11 Undefined index: root in module/product/view/view.html.php on line 78 when visiting /zenTao/www/index.php?m=product&f=view&productID=1
+
+12:15:11 Undefined index: root in module/product/view/view.html.php on line 82 when visiting /zenTao/www/index.php?m=product&f=view&productID=1
+
+12:15:11 Undefined index: root in module/product/view/view.html.php on line 103 when visiting /zenTao/www/index.php?m=product&f=view&productID=1
+
+12:18:46 Undefined index: root in module/product/view/view.html.php on line 74 when visiting /zenTao/www/index.php?m=product&f=view&productID=1
+
+12:18:46 Undefined index: root in module/product/view/view.html.php on line 78 when visiting /zenTao/www/index.php?m=product&f=view&productID=1
+
+12:18:46 Undefined index: root in module/product/view/view.html.php on line 82 when visiting /zenTao/www/index.php?m=product&f=view&productID=1
+
+12:18:46 Undefined index: root in module/product/view/view.html.php on line 103 when visiting /zenTao/www/index.php?m=product&f=view&productID=1
+
+12:18:53 Undefined index: root in module/product/view/view.html.php on line 74 when visiting /zenTao/www/index.php?m=product&f=view&productID=1
+
+12:18:53 Undefined index: root in module/product/view/view.html.php on line 78 when visiting /zenTao/www/index.php?m=product&f=view&productID=1
+
+12:18:53 Undefined index: root in module/product/view/view.html.php on line 82 when visiting /zenTao/www/index.php?m=product&f=view&productID=1
+
+12:18:53 Undefined index: root in module/product/view/view.html.php on line 103 when visiting /zenTao/www/index.php?m=product&f=view&productID=1
+
+12:24:16 Undefined variable: modules in module/story/model.php on line 1191 when visiting /zenTao/www/index.php?m=project&f=story&projectID=1
+
+12:27:08 Creating default object from empty value in module/project/model.php on line 314 when visiting /zenTao/www/index.php?m=project&f=edit&projectID=1
+
+13:16:49 Undefined offset: 1 in module/user/control.php on line 686 when visiting /zenTao/www/index.php?m=user&f=login&referer=L3plblRhby93d3cvaW5kZXgucGhw
