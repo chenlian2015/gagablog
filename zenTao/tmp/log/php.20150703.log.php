86e4bb9f21773868e3280b13bf1f8f8b7ad202c5
chenlian <chenlian2025@gmail.com>
2015-07-04 00:00:53 +0800

    submit changes by admin

---
+++ zenTao/tmp/log/php.20150703.log.php
@@ -0,0 +1,6 @@
+<?php
+ die();
+?>
+
+19:59:23 ERROR: SQLSTATE[HY000] [2002] Connection refused in framework/router.class.php on line 1523, last called by framework/router.class.php on line 1485 through function connectByPDO.
+ in framework/router.class.php on line 1567 when visiting 
